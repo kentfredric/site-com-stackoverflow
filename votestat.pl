7b1c3db6105716891aca5a48569de4c9a9d9ec11
Kent Fredric <kentfredric@gmail.com>
2009-01-12 08:42:36 +1300

    starting bare

---
+++ votestat.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE:  votestat.pl
+#
+#        USAGE:  ./votestat.pl
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Kent Fredric (theJackal), <kentfredric@gmail.com>
+#      COMPANY:  
+#      VERSION:  0.1
+#      CREATED:  12/01/09 08:42:03 NZDT
+#     REVISION:  $Id$
+#===============================================================================
+
+use strict;
+use warnings;
+use version;
+our $VERSION = qv('0.1');
+
